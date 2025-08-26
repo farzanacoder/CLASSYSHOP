20857d1f6480f9a86f13a992566edc9327a74039
farzanacoder <farzanaashif10@gmail.com>
2025-08-26 18:27:36 +0600

    Layout done

--- src/components/NextArr.jsx
+++ src/components/NextArr.jsx
@@ -4,7 +4,7 @@ import { FaLongArrowAltRight } from 'react-icons/fa'
 const NextArr = ({ className, onClick }) => {
   return (
     <div
-            className={`bg-[#00000033] w-[64px] h-[64px] flex items-center justify-center rounded-full text-black absolute top-1/3 -translate-1/3 -right-1 ${className}`}
+            className={`bg-[#00000033] w-[64px] h-[64px] flex items-center justify-center rounded-full text-black absolute top-1/2 -translate-y-1/2 z-10 cursor-pointer right-2  ${className}`}
             onClick={onClick}>
             <FaLongArrowAltRight className=' text-xl ' />
         </div>
--- src/layouts/BuyNow.jsx
+++ src/layouts/BuyNow.jsx
@@ -1,25 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../components/Container'
 import Flex from '../components/Flex'
 import ShopBrand from '../components/ShopBrand'
 import SHOP3 from '../assets/shop3.png'
 import SHOP4 from '../assets/shop4.png'
 import Image from '../components/Image'
-import Buyimg from '../assets/bnd1.png'
+import Buyimg from '../assets/buy.png'
+import Buyimg2 from '../assets/buy1.png'
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
 
 const BuyNow = () => {
+    const [activeSlide , setactiveSlide] = useState(0)
+     const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000, 
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false ,
+    afterChange: index => setactiveSlide(index)
+  }
+
+
   return (
-    <section className='w-full'>
+    <section className='w-full pt-14'>
         <Container>
             <Flex className='justify-between flex-col lg:flex-row'>
-   <div className='relative overflow-hidden group rounded-md w-[74%]'>
-        <Image className='w-full h-full duration-150 transition-transform group-hover:scale-105' src={Buyimg}/>
+   <div className='relative group rounded-md w-[74%]'>
+        <Slider {...settings}>
+          <div className="relative overflow-hidden">
+                <Image className='w-full h-full' src={Buyimg}/>
 
-        <Flex className='absolute top-0 right-0 flex-col text-left w-[50%] h-[100%] gap-1 items-start justify-center'>
-      <h1 className="lg:text-lg text-sm mb-2 w-full font-mont font-semibold">Buy women productss with low price</h1>
-      <p className="text-[20px] text-red font-semibold">₹999</p>
-      <button className='font-semibold underline hover:text-red uppercase text-base cursor-pointer duration-300'>SHOP NOW</button>
-    </Flex>
+                <Flex className={`absolute top-0 right-0 w-[45%] h-full flex-col gap-2 items-start justify-center p-4 
+                        transition-transform duration-700 ease-out
+                        ${activeSlide === 0 ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
+                  <h3 className='text-lg'>Big saving days sale</h3>
+                  <h1 className="lg:text-3xl text-md mb-2 w-full font-mont font-bold py-3.5">
+                    Buy New Trend Women Black Cotton Blend Top | top for women | women top...
+                  </h1>
+                  <div className='flex pb-2.5 gap-6'>
+                    <h3 className='text-lg'>Starting At Only</h3>
+                    <p className="text-3xl text-red font-semibold">₹1,550.00</p>
+                  </div>
+                  <button className='font-semibold rounded-md hover:bg-black text-white bg-red py-2 px-4 uppercase text-base cursor-pointer duration-300'>
+                    SHOP NOW
+                  </button>
+                </Flex>
+              </div>
+
+              {/* Slide 2 */}
+              <div className="relative overflow-hidden">
+                <Image className='w-full h-full' src={Buyimg2}/>
+                <Flex className={`absolute top-0 right-0 w-[45%] h-full flex-col gap-2 items-start justify-center p-4 
+                        transition-transform duration-700 ease-out
+                        ${activeSlide === 1 ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}>
+                  <h3 className='text-lg'>Flash Deal</h3>
+                  <h1 className="lg:text-3xl text-md mb-2 w-full font-mont font-bold py-3.5">
+                    Trendy Women Stylish Cotton Dress – Limited Time!
+                  </h1>
+                  <div className='flex pb-2.5 gap-6'>
+                    <h3 className='text-lg'>Starting At Only</h3>
+                    <p className="text-3xl text-red font-semibold">₹1,250.00</p>
+                  </div>
+                  <button className='font-semibold rounded-md hover:bg-black text-white bg-red py-2 px-4 uppercase text-base cursor-pointer duration-300'>
+                    SHOP NOW
+                  </button>
+                </Flex>
+              </div>
+        </Slider>
     </div>
 
 
